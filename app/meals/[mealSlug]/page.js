1bcbe273b2026507b88406f2bbc52feff1ce080d
Francesco De Vivo <francescodvf@yahoo.it>
2025-09-10 07:30:10 +0200

    feat: add initial routes, components, styles, and SQLite setup
    - Created basic routes and components structure
    - Added CSS modules for component styling
    - Integrated SQLite database
    - Added initdb.js with dummy meals data for fetching

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,3 @@
+export default function MealDeatailsPage() {
+  return <h1>Meal Deatails Page</h1>;
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+import MainHeaderBackground from './main-header-background';
+import Navlink from './nav-ink';
+
+export default function MainHeader() {
+
+
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
+          <Image src={logoImg} alt='A plate of food.' priority />
+          Next Level Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Navlink href='/meals'>Browse Meals</Navlink>
+            </li>
+            <li>
+              <Navlink href='/community'>Foodies Community</Navlink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
